c6a4ce56b8d95eec3ad70b1e8fb59740433096e7
Hui369 <52521232+Hui369@users.noreply.github.com>
2021-01-26 16:04:47 +0800

    Update Emulator.java

--- Emulator.java
+++ Emulator.java
@@ -9,19 +9,11 @@ import ntust.huiting.random.Generator;
  */
 public class Emulator {
 	final static int queue_number = EmulatorParameters.queue_number;
-	
-	final static double constant_deadline_time[] = {0.01, 1, 0.05};// 約束時間
-	
-	//final static double lambda = 2; // UE傳送到Edge的封包到達率	
-	final static int service_number = 3; // 服務類型數量
-	final static double lambda[] = {210, 60, 30}; // UE傳送到Edge的封包到達率
-	final static double r[] = {0.5, 1.5, 0.5}; //urllc,embb,mmtc packet UE2Edge input size (bits)
-	final static double w[] = {0.11, 1, 0.5}; //urllc,embb,mmtc packet Edge work load (cycles)
-	final static double o[] = {0.5, 1.5, 0.5}; //urllc,embb,mmtc packet Edge2UE output size ((bits)
-	final static double mu_UE = 4000; // UE傳送到Edge的服務率(傳輸率)
-	final static double mu_E = 0.3; // 在Edge的服務率(傳輸率)
-	final static double mu_EU = 6300; // Edge傳送到UE的服務率(傳輸率)
-	final static int number_packet = 1000; // 模擬封包數	
+	final static double lambda = 2; // UE傳送到Edge的封包到達率	
+	final static double mu_UE = 12; // UE傳送到Edge的服務率(傳輸率)
+	final static double mu_E = 8; // 在Edge的服務率(傳輸率)
+	final static double mu_EU = 11; // Edge傳送到UE的服務率(傳輸率)
+	final static int number_packet = 100000; // 模擬封包數	
 	static Packet packet[] = new Packet[number_packet]; // 封包
 	
 	/**
@@ -34,7 +26,7 @@ public class Emulator {
 		
 		for(int k = 0; k < queue_number; k++) {
 			// M/M/1 queue
-			m_m_1(k);// FIFO
+			m_m_1(k);
 		}
 		
 		// 顯示統計結果
@@ -46,37 +38,21 @@ public class Emulator {
 	 * 設定每個封包的到達時間和服務時間
 	 */
 	private static void initial_packet() {
-		// 亂數產生器// Random number generator
+		// 亂數產生器
 		Generator g = new Generator();
 		
-		// 當下時間點// Current time
-		double current_time[] = {0, 0, 0};
+		// 當下時間點
+		double current_time = 0;
 		
-		//產生number_packet個封包//Generate number_packet's packets
+		//產生number_packet個封包
 		for(int i = 0; i < number_packet; i++) {
-			int service_type_gen = 0;
-			double current_time_gen = current_time[service_type_gen];
-			for(int j = 0; j < service_number; j++) {
-				if(current_time_gen > current_time[j]) {
-					service_type_gen = j;
-					current_time_gen = current_time[service_type_gen];
-				}
-			}
-			double inter_arrival_time = g.getRandomNumber_Poisson(lambda[service_type_gen]);
-			double arrival_time = current_time[service_type_gen] += inter_arrival_time;
-			
-			double r_task_size = g.getRandomNumber_Normal(r[service_type_gen]);
-			double w_task_size = g.getRandomNumber_Normal(w[service_type_gen]);
-			double o_task_size = g.getRandomNumber_Normal(o[service_type_gen]);
-			
+			double inter_arrival_time = g.getRandomNumber_Exponential(lambda);
+			double arrival_time = current_time += inter_arrival_time;
 			double service_time[] = new double[queue_number];
-			service_time[0] = (r_task_size / (mu_UE));
-			service_time[1] = (w_task_size / (mu_UE));
-			service_time[2] = (o_task_size / (mu_UE));
-			
-			double deadline_time = arrival_time + constant_deadline_time[service_type_gen];
-			
-			packet[i] = new Packet(i + 1, service_type_gen, r_task_size, w_task_size, o_task_size, arrival_time, service_time, deadline_time);
+			service_time[0] = g.getRandomNumber_Exponential(mu_UE);
+			service_time[1] = g.getRandomNumber_Exponential(mu_E);
+			service_time[2] = g.getRandomNumber_Exponential(mu_EU);
+			packet[i] = new Packet(arrival_time, service_time);
 		}
 	}
 	
@@ -122,19 +98,12 @@ public class Emulator {
 		double total_waiting_time[] = new double[queue_number];
 		double total_processing_time = 0;
 		double simulation_time = packet[number_packet - 1].departure_time[queue_number - 1];
-		double QV = 0;//計算已延遲封包(超過deadline_time)
 		
 		// 顯示和統計每一個封包的時間
 		for(int i = 0; i < number_packet; i++) {
-			
-			if(packet[i].deadline_time - packet[i].departure_time[queue_number - 1]< 0) {
-				QV++;
-			}
-			
 			for(int k = 0; k < queue_number; k++) {
 				total_service_time[k] += packet[i].service_time[k];
 				total_waiting_time[k] += packet[i].waiting_time[k];
-				
 			}
 			total_processing_time += packet[i].departure_time[queue_number - 1] - packet[i].arrival_time[0];
 			
@@ -148,6 +117,5 @@ public class Emulator {
 			System.out.println("Queue_" + (k+1) + "等候長度: " + (total_waiting_time[k]/simulation_time));
 		}
 		System.out.println("平均每個封包的處理時間: " + (total_processing_time/number_packet));
-		System.out.println("超過deadline的比例: " + (QV/number_packet));
 	}
 }
